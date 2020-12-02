2931d6b24069db54b667472ab9624837bc01d5b2
Dan Aita <59552043+aitaDev@users.noreply.github.com>
2020-12-02 14:48:52 -0600

    swipe and delete favorites

--- redux/favorites.js
+++ redux/favorites.js
@@ -8,6 +8,9 @@ export const favorites = (state = [], action) => {
 			}
 			return state.concat(action.payload);
 
+		case ActionTypes.DELETE_FAVORITE:
+			return state.filter((favorite) => favorite !== action.payload);
+		// important, filters array, returns new array with actions that dont equal payload.
 		default:
 			return state;
 	}
