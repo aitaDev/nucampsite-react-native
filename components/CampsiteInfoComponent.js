521801d2365db8f6eb304d637a37ea21582aef8b
Dan Aita <59552043+aitaDev@users.noreply.github.com>
2020-12-05 13:41:56 -0600

    W3 Assignment,removed modal, added ratings gesture

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -67,14 +67,18 @@ function RenderCampsite(props) {
 	const view = React.createRef();
 
 	const recognizeDrag = ({ dx }) => (dx < -200 ? true : false);
+	const recognizeComment = ({ dx }) => (dx < 200 ? true : false);
 
-    const panResponder = PanResponder.create({
-        onStartShouldSetPanResponder: () => true,
-        onPanResponderGrant: () => {
-            view.current.rubberBand(1000)
-            .then(endState => console.log(endState.finished ? 'finished' : 'canceled'));
-        },
-        onPanResponderEnd: (e, gestureState) => {
+	const panResponder = PanResponder.create({
+		onStartShouldSetPanResponder: () => true,
+		onPanResponderGrant: () => {
+			view.current
+				.rubberBand(1000)
+				.then((endState) =>
+					console.log(endState.finished ? 'finished' : 'canceled')
+				);
+		},
+		onPanResponderEnd: (e, gestureState) => {
 			console.log('pan responder end', gestureState);
 			if (recognizeDrag(gestureState)) {
 				Alert.alert(
@@ -96,6 +100,8 @@ function RenderCampsite(props) {
 					],
 					{ cancelable: false }
 				);
+			} else if (recognizeComment(gestureState)) {
+				props.onShowModal();
 			}
 			return true;
 		},
