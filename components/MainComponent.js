323ac53d4d9b4a06bd25b9e91fdb637152ef4b43
Dan Aita <59552043+aitaDev@users.noreply.github.com>
2020-12-12 13:37:57 -0600

    changed netInfo to  async

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -331,26 +331,25 @@ class Main extends Component {
 		this.props.fetchComments();
 		this.props.fetchPartners();
 		this.props.fetchPromotions();
-
-		NetInfo.fetch().then((connectionInfo) => {
-			Platform.OS === 'ios'
-				? Alert.alert(
-						'Initial Network Connectivity Type: ',
-						connectionInfo.type
-				  )
-				: ToastAndroid.show(
-						'Initial Network Connectivity Type: ' + connectionInfo.type,
-						ToastAndroid.LONG
-				  );
-		});
-
-		this.unsubscribeNetInfo = NetInfo.addEventListener((connectionInfo) => {
-			this.handleConnectivityChange(connectionInfo);
-		});
+		this.showNetInfo();
 	}
 
 	componentWillUnmount() {
 		this.unsubscribeNetInfo();
+	}
+
+	async showNetInfo() {
+		const connectionInfo = await NetInfo.fetch();
+		Platform.OS === 'ios'
+			? Alert.alert('Initial Network Connectivity Type: ', connectionInfo.type)
+			: ToastAndroid.show(
+					'Initial Network Connectivity Type: ' + connectionInfo.type,
+					ToastAndroid.LONG
+			  );
+
+		unsubscribeNetInfo = NetInfo.addEventListener((showNetInfo) => {
+			this.handleConnectivityChange(connectionInfo);
+		});
 	}
 
 	handleConnectivityChange = (connectionInfo) => {
